012affaf28902c85ef7bc8afcb915bc3980cfe33
siddhantkourav <siddhant2391@gmail.com>
2021-03-23 22:05:08 +0530

    first

---
+++ TokenAuthenticationWEBAPI/Models/ClientMasterRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TokenAuthenticationWEBAPI.Models
+{
+    public class ClientMasterRepository :IDisposable
+    {
+        SECURITY_DBEntities context = new SECURITY_DBEntities();
+
+        //This method is used to check and validate the Client credentials
+        public ClientMaster ValidateClient(string ClientID, string ClientSecret)
+        {
+            return context.ClientMasters.FirstOrDefault(user => user.ClientId == ClientID
+            && user.ClientSecret == ClientSecret);
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
---
+++ TokenAuthenticationWEBAPI/Models/UserMasterRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TokenAuthenticationWEBAPI.Models
+{
+    public class UserMasterRepository : IDisposable
+    {
+        SECURITY_DBEntities context = new SECURITY_DBEntities();
+        public UserMaster ValidateUser(string username, string password)
+        {
+            return context.UserMasters.FirstOrDefault(x => x.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && x.UserPassword == password);
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
